e6043372e9f9e9c5dbdd0a6e39199c71368a2f7f
Dmitri S. Cunev <cunevnew@gmail.com>
2023-09-10 01:31:04 +0300

    update key

--- src/openai.ts
+++ src/openai.ts
@@ -1,4 +1,4 @@
-const OPENAI_API_KEY = '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'
+const OPENAI_API_KEY = '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'
 process.env.OPENAI_API_KEY = OPENAI_API_KEY
 import OpenAI from 'openai'
 
