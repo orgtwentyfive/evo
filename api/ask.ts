7de1aae5ea26502dd6f19607eb5d66e1946092cc
Dmitri S. Cunev <cunevnew@gmail.com>
2023-09-09 12:33:19 +0300

    update 2

--- api/ask.ts
+++ api/ask.ts
@@ -1,9 +1,13 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { filterEmbeddings } from '../src/filterEmbeddings'
+import { getTop } from '../src/getTop'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     if (!request.url) return response.status(400)
     const question = request.body.question
     const conversation_id = request.body.conversation_id
+    const top = await getTop(question, 40)
+    const filtered = await filterEmbeddings(top, question)
 
-    return response.status(200).json({ question, conversation_id })
+    return response.status(200).json(filtered)
 }
