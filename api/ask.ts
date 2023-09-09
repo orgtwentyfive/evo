2076da76335373060a37cf50a489ae7c452cda4d
Dmitri S. Cunev <cunevnew@gmail.com>
2023-09-09 12:31:53 +0300

    api update

--- api/ask.ts
+++ api/ask.ts
@@ -2,11 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     if (!request.url) return response.status(400)
+    const question = request.body.question
+    const conversation_id = request.body.conversation_id
 
-    const url = new URL(request.url, `http://${request.headers.host}`)
-    const { searchParams } = url
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'My default title'
-
-    return response.status(200).json({ title })
+    return response.status(200).json({ question, conversation_id })
 }
