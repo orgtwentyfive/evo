ab349ba02ff072865a7148737a289310f1af76f8
Dmitri S. Cunev <cunevnew@gmail.com>
2023-09-10 12:04:25 +0300

    tt

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,45 @@
+import chalk from 'chalk'
 import { answer } from './answer'
 import { getData, setData } from './db'
 import { filterEmbeddings } from './filterEmbeddings'
 import { data, getTop } from './getTop'
 
 async function main() {
-    const converstationId = Math.random().toString()
-
-    let conversation = await getData(converstationId)
+    let conversation: any = { articleIds: [], conversations: [] }
 
     const question = 'cum fac cazier?'
     // // const question = 'am nevoie de un extras din registru unitatilor de drept?'
     // // ce documente am nevoie?
     // // cat costa?
-    if (!conversation) {
-        conversation = { articleIds: [], conversations: [] }
-    }
 
     if (!conversation.articleIds!.length) {
         const top = await getTop(question, 40)
         const filtered = await filterEmbeddings(top, question)
-        await setData(converstationId, {
-            articleIds: filtered,
-            conversations: conversation.conversations,
-        })
+        conversation.article_ids = filtered
+        console.log(chalk.red('Step 1'))
         console.log(
             `Am gasit informatie in urmatoarele articole\n${filtered.map((e) => `https://servicii.gov.md/ro/service/${e}`).join('\n')}`,
         )
     }
 
-    await setData(converstationId, {
-        conversations: [
-            ...conversation.conversations!,
-            {
-                content: `${question}`,
-                role: 'user',
-            },
-        ],
-    })
-
-    conversation = await getData(converstationId)
-
-    console.log(conversation)
+    conversation.conversations = [
+        {
+            content: `${question}`,
+            role: 'user',
+        },
+    ]
+    console.log(chalk.red('Step 2'))
     console.log(`Caut prin articole...`)
     const streams = await answer(conversation.articleIds!, conversation.conversations!)
     let answers = ''
     for await (const stream of streams) {
         answers += stream.choices[0].delta.content
-        console.clear()
-        console.log(answers)
+        // console.clear()
     }
+    console.log(chalk.red('Step 3'))
 
-    await setData(converstationId, {
-        conversations: [...conversation.conversations!, { content: answers, role: 'assistant' }],
-    })
+    console.log(answers)
+
     // console.log(answers)
 }
 
